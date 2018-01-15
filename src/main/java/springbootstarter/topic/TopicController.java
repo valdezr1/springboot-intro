a8da0eaea6ad99910014d281bb860af33745784e
Rafael Valdez <valdezr1@outlook.com>
2018-01-15 12:48:48 -0800

    TopicController Created to return hard-coded List of Topics

---
+++ src/main/java/springbootstarter/topic/TopicController.java
@@ -0,0 +1,21 @@
+package springbootstarter.topic;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicController {
+
+    @RequestMapping("/topics")
+    public List<Topic> getAllTopics(){
+        return Arrays.asList(
+                new Topic("spring", "Spring Framework", "Spring Framework Description"),
+                new Topic ("java", "Core Java", "Core Java Description"),
+                new Topic ("algorithms", "Advanced Algorithms", "Advanced Algorithms Description")
+        );
+    }
+
+}
