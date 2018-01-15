9760d8e32ee934e89396114f8ad21dfe6121ece4
Rafael Valdez <valdezr1@outlook.com>
2018-01-15 13:34:29 -0800

    Created TopicService to house hard-coded Topics in a List. Initialized getAllTopics in service

--- src/main/java/springbootstarter/topic/TopicController.java
+++ src/main/java/springbootstarter/topic/TopicController.java
@@ -1,5 +1,6 @@
 package springbootstarter.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,13 +10,12 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics(){
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic ("java", "Core Java", "Core Java Description"),
-                new Topic ("algorithms", "Advanced Algorithms", "Advanced Algorithms Description")
-        );
+        return topicService.getAllTopics();
     }
 
 }
---
+++ src/main/java/springbootstarter/topic/TopicService.java
@@ -0,0 +1,20 @@
+package springbootstarter.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Arrays;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = Arrays.asList(
+            new Topic("spring", "Spring Framework", "Spring Framework Description"),
+            new Topic ("java", "Core Java", "Core Java Description"),
+            new Topic ("algorithms", "Advanced Algorithms", "Advanced Algorithms Description")
+            );
+
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+}
