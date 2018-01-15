871b76db34f357fcb09e657cefff50052ac23dd4
Rafael Valdez <valdezr1@outlook.com>
2018-01-15 12:13:41 -0800

    First commit - server initialized and running

---
+++ src/main/java/hello/Intro.java
@@ -0,0 +1,13 @@
+package hello;//package java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Intro {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Intro.class, args);
+    }
+}
+
